10ee8246ae8485ebc2bd5ec810b55881d54fdf2c
ihornekhaienko <70515099+ihornekhaienko@users.noreply.github.com>
2020-08-31 17:36:19 +0300

    Add files via upload

---
+++ MyNotepad/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace MyNotepad
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuNewWindow = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage = new System.Windows.Forms.TabPage();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Window;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile,
+            this.formatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuFile
+            // 
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuNew,
+            this.MenuNewWindow,
+            this.MenuOpen,
+            this.MenuSave,
+            this.MenuSaveAs,
+            this.toolStripMenuItem1,
+            this.MenuExit});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(46, 26);
+            this.MenuFile.Text = "&File";
+            // 
+            // MenuNew
+            // 
+            this.MenuNew.Name = "MenuNew";
+            this.MenuNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.MenuNew.Size = new System.Drawing.Size(271, 26);
+            this.MenuNew.Text = "&New";
+            this.MenuNew.Click += new System.EventHandler(this.MenuNew_Click);
+            // 
+            // MenuNewWindow
+            // 
+            this.MenuNewWindow.Name = "MenuNewWindow";
+            this.MenuNewWindow.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.N)));
+            this.MenuNewWindow.Size = new System.Drawing.Size(271, 26);
+            this.MenuNewWindow.Text = "&New window";
+            this.MenuNewWindow.Click += new System.EventHandler(this.MenuNewWindow_Click);
+            // 
+            // MenuOpen
+            // 
+            this.MenuOpen.Name = "MenuOpen";
+            this.MenuOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.MenuOpen.Size = new System.Drawing.Size(271, 26);
+            this.MenuOpen.Text = "&Open...";
+            this.MenuOpen.Click += new System.EventHandler(this.MenuOpen_Click);
+            // 
+            // MenuSave
+            // 
+            this.MenuSave.Name = "MenuSave";
+            this.MenuSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.MenuSave.Size = new System.Drawing.Size(271, 26);
+            this.MenuSave.Text = "&Save";
+            this.MenuSave.Click += new System.EventHandler(this.MenuSave_Click);
+            // 
+            // MenuSaveAs
+            // 
+            this.MenuSaveAs.Name = "MenuSaveAs";
+            this.MenuSaveAs.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.MenuSaveAs.Size = new System.Drawing.Size(271, 26);
+            this.MenuSaveAs.Text = "&Save as...";
+            this.MenuSaveAs.Click += new System.EventHandler(this.MenuSaveAs_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(268, 6);
+            // 
+            // MenuExit
+            // 
+            this.MenuExit.Name = "MenuExit";
+            this.MenuExit.Size = new System.Drawing.Size(271, 26);
+            this.MenuExit.Text = "&Exit";
+            this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFont,
+            this.MenuColor});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(70, 26);
+            this.formatToolStripMenuItem.Text = "&Format";
+            // 
+            // MenuFont
+            // 
+            this.MenuFont.Name = "MenuFont";
+            this.MenuFont.Size = new System.Drawing.Size(137, 26);
+            this.MenuFont.Text = "&Font...";
+            this.MenuFont.Click += new System.EventHandler(this.MenuFont_Click);
+            // 
+            // MenuColor
+            // 
+            this.MenuColor.Name = "MenuColor";
+            this.MenuColor.Size = new System.Drawing.Size(137, 26);
+            this.MenuColor.Text = "&Color...";
+            this.MenuColor.Click += new System.EventHandler(this.MenuColor_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Window;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.menuStrip1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 30);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 30);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 420);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage
+            // 
+            this.tabPage.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPage.Controls.Add(this.textBox);
+            this.tabPage.Location = new System.Drawing.Point(4, 25);
+            this.tabPage.Name = "tabPage";
+            this.tabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage.Size = new System.Drawing.Size(792, 391);
+            this.tabPage.TabIndex = 0;
+            this.tabPage.Tag = "";
+            this.tabPage.Text = "Untitled";
+            // 
+            // textBox
+            // 
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(3, 3);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(786, 385);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "MyNotepad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage.ResumeLayout(false);
+            this.tabPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuFile;
+        private System.Windows.Forms.ToolStripMenuItem MenuNew;
+        private System.Windows.Forms.ToolStripMenuItem MenuNewWindow;
+        private System.Windows.Forms.ToolStripMenuItem MenuOpen;
+        private System.Windows.Forms.ToolStripMenuItem MenuSave;
+        private System.Windows.Forms.ToolStripMenuItem MenuSaveAs;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem MenuExit;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuFont;
+        private System.Windows.Forms.ToolStripMenuItem MenuColor;
+    }
+}
+
---
+++ MyNotepad/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MyNotepad
+{
+    public partial class MainForm : Form
+    {
+        static int count = 1;
+        string path;
+        static FontDialog fd = new FontDialog();
+        static ColorDialog cd = new ColorDialog();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// MainForm load event handler, sets initial values.
+        /// </summary>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            path = null;
+            fd.Font = textBox.Font;
+            cd.Color = textBox.ForeColor;
+            ActiveControl = textBox;
+        }
+
+        /// <summary>
+        /// MainForm closing event handler, calls method that check if the file is saved.
+        /// </summary>
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Unsave();
+        }
+
+        /// <summary>
+        /// Menu new window clicked event handler, opens a new window.
+        /// </summary>
+        private void MenuNewWindow_Click(object sender, EventArgs e)
+        {
+            MainForm newMainForm = new MainForm();
+            newMainForm.Show();
+        }
+
+        /// <summary>
+        /// Menu open clicked event handler, opens file.
+        /// </summary>
+        private void MenuOpen_Click(object sender, EventArgs e)
+        {
+            Unsave();
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = openFileDialog.FileName;
+
+                    var fileStream = openFileDialog.OpenFile();
+
+                    StreamReader reader = new StreamReader(fileStream);
+                    tabControl1.SelectedTab.Text = Path.GetFileName(path);
+                    tabControl1.SelectedTab.Tag = path;
+                    tabControl1.SelectedTab.Controls[0].Text = reader.ReadToEnd();
+                    reader.Close();
+                    path = string.Empty;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Menu save clicked event handler, calls Save method.
+        /// </summary>
+        private void MenuSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        /// <summary>
+        /// Menu save as clicked event handler, calls Save As method.
+        /// </summary>
+        private void MenuSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        /// <summary>
+        /// Menu exit clicked event handler, calls logout dialog.
+        /// </summary>
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to go out?", Text,
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// Menu new clicked event handler, creates new page.
+        /// </summary>
+        private void MenuNew_Click(object sender, EventArgs e)
+        {
+            string name = "Untitled" + count.ToString();
+
+            TextBox tb = new TextBox();
+            tb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            tb.Multiline = true;
+            tb.Dock = System.Windows.Forms.DockStyle.Fill;
+            tb.Name = "textBox" + count.ToString();
+            tb.Size = new System.Drawing.Size(786, 15);
+            tb.TabIndex = 0;
+            tb.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+
+            TabPage tp = new TabPage(name);
+            tp.BackColor = System.Drawing.SystemColors.Window;
+            tp.Controls.Add(tb);
+            tp.Name = "tabPage" + count.ToString();
+            tp.Padding = new System.Windows.Forms.Padding(3);
+            tp.Size = new System.Drawing.Size(792, 391);
+            tp.TabIndex = 0;
+            tp.Tag = string.Empty;
+
+            tabControl1.Controls.Add(tp);
+            tabControl1.SelectedIndex = tabControl1.Controls.Count - 1;
+            ActiveControl = tabControl1.SelectedTab.Controls[0];
+            count++;
+        }
+
+        /// <summary>
+        /// TextBox text changed event handler, sets a marker that the file has changed.
+        /// </summary>
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab.Text[0] != '*')
+            {
+                tabControl1.SelectedTab.Text = "*" + tabControl1.SelectedTab.Text;
+            }
+        }
+
+        /// <summary>
+        /// TextBox text changed event handler, calls font dialog and sets font.
+        /// </summary>
+        private void MenuFont_Click(object sender, EventArgs e)
+        {
+            if (fd.ShowDialog() == DialogResult.Cancel)
+                return;
+            foreach (TabPage i in tabControl1.Controls)
+            {
+                i.Controls[0].Font = fd.Font;
+            }
+        }
+
+        /// <summary>
+        /// TextBox text changed event handler, calls color dialog and sets color.
+        /// </summary>
+        private void MenuColor_Click(object sender, EventArgs e)
+        {
+            if (cd.ShowDialog() == DialogResult.Cancel)
+                return;
+            foreach (TabPage i in tabControl1.Controls)
+            {
+                i.Controls[0].ForeColor = cd.Color;
+            }
+        }
+
+        /// <summary>
+        /// Method, that call Save-method for all pages.
+        /// </summary>
+        private void SaveAll()
+        {
+            foreach (TabPage i in tabControl1.Controls)
+            {
+                tabControl1.SelectedTab = i;
+                Save();
+            }
+        }
+
+        /// <summary>
+        /// Method, that save current file.
+        /// </summary>
+        private void Save()
+        {
+            if (tabControl1.SelectedTab.Text[0] != '*')
+            {
+                return;
+            }
+            if (tabControl1.SelectedTab.Tag.ToString() == string.Empty)
+            {
+                SaveAs();
+                return;
+            }
+
+            StreamWriter writer = new StreamWriter(tabControl1.SelectedTab.Tag.ToString());
+            writer.WriteLine(tabControl1.SelectedTab.Controls[0].Text);
+            writer.Close();
+            tabControl1.SelectedTab.Text = tabControl1.SelectedTab.Text.Remove(0,1);
+        }
+
+        /// <summary>
+        /// Method, that call save file dialog and save file.
+        /// </summary>
+        private void SaveAs()
+        {
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = saveFileDialog.FileName;
+
+                    var fileStream = saveFileDialog.OpenFile();
+
+                    StreamWriter writer = new StreamWriter(fileStream);
+                    writer.WriteLine(tabControl1.SelectedTab.Controls[0].Text);
+                    tabControl1.SelectedTab.Tag = path;
+                    writer.Close();
+                    tabControl1.SelectedTab.Text = Path.GetFileName(path);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method, that check if the file is saved.
+        /// </summary>
+        private void Unsave()
+        {
+            foreach (TabPage i in tabControl1.Controls)
+            {
+                if (i.Text[0] == '*' && i.Controls[0].Text.Length > 0)
+                {
+                    
+                    if (MessageBox.Show("Do you want to save changes to your text?", this.Text,
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        SaveAll();
+                    }
+                }
+            }
+        }
+    }
+}
